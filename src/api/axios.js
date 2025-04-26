e9d15f1a45aef6f030539466080fb0b2e04ce6ff
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2025-04-26 09:25:27 -0500

    Se ajusta front para subir a vercel

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://parcial-back-nu.vercel.app/api' });
+const API = axios.create({ baseURL: 'https://parcial-front-kappa.vercel.app/api' });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
